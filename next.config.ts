75573d87f1674e31e912dc4f6c207a76a7961e10
URA-NETWORKS Response <uranetworksresponse@gmail.com>
2025-10-03 09:26:55 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'files.catbox.moe',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
